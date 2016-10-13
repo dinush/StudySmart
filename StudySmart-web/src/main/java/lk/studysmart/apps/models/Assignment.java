64bc071268dfbaba6b13f86a5bc869f4d2c89cfc
Sisinda Dinusha <sisindaa@gmail.com>
2016-10-13 08:41:24 +0530

    Assignment model update

--- StudySmart-web/src/main/java/lk/studysmart/apps/models/Assignment.java
+++ StudySmart-web/src/main/java/lk/studysmart/apps/models/Assignment.java
@@ -33,7 +33,8 @@ import javax.xml.bind.annotation.XmlTransient;
 @NamedQueries({
     @NamedQuery(name = "Assignment.findAll", query = "SELECT a FROM Assignment a"),
     @NamedQuery(name = "Assignment.findByName", query = "SELECT a FROM Assignment a WHERE a.name = :name"),
-    @NamedQuery(name = "Assignment.findByMax", query = "SELECT a FROM Assignment a WHERE a.max = :max")})
+    @NamedQuery(name = "Assignment.findByMax", query = "SELECT a FROM Assignment a WHERE a.max = :max"),
+    @NamedQuery(name = "Assignment.findByClass2", query = "SELECT a FROM Assignment a WHERE a.class1 = :class2")})
 public class Assignment implements Serializable {
 
     private static final long serialVersionUID = 1L;
