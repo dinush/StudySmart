2a3707eb79c414af3251d020bd1f8bd6f54711d6
Sisinda Dinusha <sisindaa@gmail.com>
2017-01-11 13:06:07 +0530

    Remove user implementation

---
+++ StudySmart-web/src/main/java/lk/studysmart/apps/service/service/Admin.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.studysmart.apps.service.service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import lk.studysmart.apps.models.User;
+
+/**
+ *
+ * @author dinush
+ */
+@Stateless
+@Path("admin")
+public class Admin {
+    
+    @PersistenceUnit(unitName = "lk.studysmart_StudySmart-web_war_1.0-SNAPSHOTPU")
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("lk.studysmart_StudySmart-web_war_1.0-SNAPSHOTPU");
+
+    @Resource
+    UserTransaction utx;
+
+    @PersistenceContext
+    EntityManager em;
+    
+    @DELETE
+    @Path("removeuser/{username}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void removeUser(@PathParam("username") String username,
+            @Context HttpServletRequest request) {
+        
+        User deleteUser = em.find(User.class, username);
+        
+        try {
+            utx.begin();
+            em.remove(deleteUser);
+            utx.commit();
+        } catch (RollbackException | HeuristicMixedException | HeuristicRollbackException | SecurityException | IllegalStateException | SystemException | NotSupportedException ex) {
+            Logger.getLogger(Admin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
