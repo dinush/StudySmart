f722c3213b2c0caa442c26abed737ea4d1539347
KaveeshBaddage <kbaddage98@gmail.com>
2017-01-15 16:40:25 +0530

    QuizTeacher

--- quiz.sql
+++ quiz.sql
@@ -1,28 +1,32 @@
--- MySQL dump 10.14  Distrib 5.5.52-MariaDB, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.18
+-- https://www.phpmyadmin.net
 --
--- Host: localhost    Database: StudySmart
--- ------------------------------------------------------
--- Server version	5.5.52-MariaDB
+-- Host: localhost
+-- Generation Time: Jan 15, 2017 at 11:03 AM
+-- Server version: 5.5.52-MariaDB
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `StudySmart`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `quiz`
 --
 
-DROP TABLE IF EXISTS `quiz`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `quiz` (
+CREATE TABLE IF NOT EXISTS `quiz` (
   `Subject` varchar(25) NOT NULL,
   `idQuiz` int(11) NOT NULL AUTO_INCREMENT,
   `question` varchar(500) NOT NULL,
@@ -34,29 +38,37 @@ CREATE TABLE `quiz` (
   `username` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`idQuiz`),
   KEY `username` (`username`),
-  KEY `subject_index` (`Subject`),
-  CONSTRAINT `quiz_subject` FOREIGN KEY (`Subject`) REFERENCES `subject` (`idSubject`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `Quiz_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `subject_index` (`Subject`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `quiz`
 --
 
-LOCK TABLES `quiz` WRITE;
-/*!40000 ALTER TABLE `quiz` DISABLE KEYS */;
-INSERT INTO `quiz` VALUES ('002',1,'1','11','12','13','14','12','teacher'),('002',2,'2','21','22','23','24','22','teacher'),('002',3,'3','31','32','33','34','34','teacher'),('002',4,'4','41','42','43','43','42','teacher'),('002',5,'5','51','52','53','54','51','teacher'),('002',6,'1','11','12','13','14','12','teacher'),('002',7,'2','21','22','23','24','22','teacher'),('002',8,'3','31','32','33','34','33','teacher'),('002',9,'4','41','42','43','44','42','teacher'),('002',10,'5','51','52','53','54','51','teacher');
-/*!40000 ALTER TABLE `quiz` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `quiz` (`Subject`, `idQuiz`, `question`, `option1`, `option2`, `option3`, `option4`, `answers`, `username`) VALUES
+('002', 1, '1', '11', '12', '13', '14', '12', 'teacher'),
+('002', 2, '2', '21', '22', '23', '24', '22', 'teacher'),
+('002', 3, '3', '31', '32', '33', '34', '34', 'teacher'),
+('002', 4, '4', '41', '42', '43', '43', '42', 'teacher'),
+('002', 5, '5', '51', '52', '53', '54', '51', 'teacher'),
+('002', 6, '1', '11', '12', '13', '14', '12', 'teacher'),
+('002', 7, '2', '21', '22', '23', '24', '22', 'teacher'),
+('002', 8, '3', '31', '32', '33', '34', '33', 'teacher'),
+('002', 9, '4', '41', '42', '43', '44', '42', 'teacher'),
+('002', 10, '5', '51', '52', '53', '54', '51', 'teacher'),
+('001', 11, 'quizzzzzz', 'opt1', 'opt2', 'opt3', 'opt4', '2', 'teacher');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD CONSTRAINT `Quiz_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `quiz_subject` FOREIGN KEY (`Subject`) REFERENCES `subject` (`idSubject`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-01-14  1:03:59
